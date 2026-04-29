5bc5d5b55e3ceaf06e2ed331127bd857ce744df2
ganesh <ulidraganesh8@gmail.com>
2026-04-29 17:56:18 +0530

    removed unnecessary packages

--- projectgns/settings.py
+++ projectgns/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-q@w=+a0t*i%tja@n497*^ted+swg+%vs+#rjs0%-*e*cg_=9e!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -121,6 +121,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, 'static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
